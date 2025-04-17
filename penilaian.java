1ab215fbe42a94069f291b59b31b20a6728144f5
Fira <izzadhafirafanani@gmail.com>
2025-04-17 09:38:31 +0700

    penilaian

--- penilaian.java
+++ penilaian.java
@@ -1,16 +1,20 @@
 public class penilaian {
+    mahasiswa mahasiswa;
+    matakuliah matakuliah;
     double nilaiTugas, nilaiUTS, nilaiUAS, nilaiAkhir;
 
-    public penilaian() {
-        this.mahasiswa = mahasiswa;
-        this.mataKuliah = mataKuliah;
-        this.nilaiTugas = nilaiTugas;
-        this.nilaiUTS = nilaiUTS;
-        this.nilaiUAS = nilaiUAS;
-        this.nilaiAkhir = nilaiAkhir;
+    public penilaian(mahasiswa mhs, matakuliah mk, double tugas, double uts, double uas) {
+        this.mahasiswa = mhs;
+        this.matakuliah = mk;
+        this.nilaiTugas = tugas;
+        this.nilaiUTS = uts;
+        this.nilaiUAS = uas;
+        hitungNilaiAkhir();
     }
-    
     void hitungNilaiAkhir() {
-        
+        nilaiAkhir = (nilaiTugas*0.3)+(nilaiUTS*0.3)+(nilaiUAS*0.4);
+    }
+    void tampilPenilaian() {
+        System.out.println(mahasiswa.nama + " | " + matakuliah.namaMK + " | Nilai Akhir: " + nilaiAkhir);
     }
 }
